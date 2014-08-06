a6f659dda1c1e6ce1862c80a2188dbcc1a1f60bd
Rhys Powell <rhys@rpowell.me>
2014-08-06 13:55:48 +1000

    Basic view controller layout

---
+++ Lap Timer/Controllers/LTTimerViewController.m
@@ -0,0 +1,22 @@
+//
+//  LTTimerViewController.m
+//  Lap Timer
+//
+//  Created by Rhys Powell on 6/08/2014.
+//  Copyright (c) 2014 Rhys Powell. All rights reserved.
+//
+
+#import "LTTimerViewController.h"
+
+@interface LTTimerViewController ()
+
+@end
+
+@implementation LTTimerViewController
+
+- (IBAction)unwindFromTimesView:(UIStoryboardSegue *)segue
+{
+
+}
+
+@end
